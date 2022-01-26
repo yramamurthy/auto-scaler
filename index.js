3147d651ae7f840794d84e4650d0fa5b56d44ee5
Yuvaraj Ramamurthy <yuvaraj.ramamurthy@gmail.com>
2022-01-26 12:41:27 +0530

    logs

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ async function autoScale(fireDate) {
 
     // loop through the applications configured to auto scale
     for (let appPlan in appPlans) {
-        console.log(`platform ${appPlans[appPlan].platform.name} for the app ${appPlans[appPlan].app_name} token ${appPlans[appPlan].platform.token}`)
+        // console.log(`platform ${appPlans[appPlan].platform.name} for the app ${appPlans[appPlan].app_name} token ${appPlans[appPlan].platform.token}`)
         try {
             // check for platform 
             if (appPlans[appPlan].platform === undefined || appPlans[appPlan].platform.name === undefined) {
